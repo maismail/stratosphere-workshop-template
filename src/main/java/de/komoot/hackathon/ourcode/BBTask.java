ef3f7e83d1648f20c6bc37edfa39e12483c0f29f
Laszlo Dudas <ldudas@dms.sztaki.hu>
2013-08-26 14:30:48 +0200

    BBTask done

---
+++ src/main/java/de/komoot/hackathon/ourcode/BBTask.java
@@ -0,0 +1,86 @@
+package de.komoot.hackathon.ourcode;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+
+import de.komoot.hackathon.openstreetmap.GeometryModule;
+import de.komoot.hackathon.openstreetmap.JsonGeometryEntity;
+import eu.stratosphere.pact.common.stubs.Collector;
+import eu.stratosphere.pact.common.stubs.MapStub;
+import eu.stratosphere.pact.common.type.PactRecord;
+import eu.stratosphere.pact.common.type.base.PactDouble;
+import eu.stratosphere.pact.common.type.base.PactList;
+import eu.stratosphere.pact.common.type.base.PactPair;
+import eu.stratosphere.pact.common.type.base.PactString;
+
+public class BBTask extends MapStub {
+
+	
+	private static class DoubleDoublePactPair extends PactPair<PactDouble, PactDouble> {
+		public DoubleDoublePactPair() {}
+		
+		public DoubleDoublePactPair(double first, double second) {
+			super(new PactDouble(first), new PactDouble(second));
+		}
+		
+		public DoubleDoublePactPair(PactDouble first, PactDouble second) {
+			super(first, second);
+		}
+		
+	}
+	
+	public static class PactCoordinatesList extends PactList<DoubleDoublePactPair> {
+		public PactCoordinatesList() {}
+		
+		public PactCoordinatesList(DoubleDoublePactPair... coordinates) {
+			for (DoubleDoublePactPair coordinate : coordinates) {
+				add(coordinate);
+			}
+		}
+		
+		public void addPair(double first, double second) {
+			add(new DoubleDoublePactPair(first, second));
+		}
+	}
+	
+	private final ObjectMapper mapper;
+	private final PactRecord outputRecord = new PactRecord();
+	private final PactString id = new PactString();
+	private final PactCoordinatesList coordinates = new PactCoordinatesList();
+	private final PactCoordinatesList envelope = new PactCoordinatesList();
+
+	
+	public BBTask() {
+		mapper = new ObjectMapper();
+		mapper.registerModule(new GeometryModule());
+		mapper.configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false);
+	}
+	
+	@Override
+	public void map(PactRecord record, Collector<PactRecord> collector)
+			throws Exception {
+		PactString line = record.getField(0, PactString.class);
+		JsonGeometryEntity<Geometry> elem =  
+				mapper.readValue(line.getValue(), JsonGeometryEntity.class);
+		
+		id.setValue(elem.getId());
+		coordinates.clear();
+		for(Coordinate cr : elem.getGeometry().getCoordinates()) {
+			coordinates.addPair(cr.x, cr.y);
+		}
+		
+		outputRecord.setField(0, id);
+		outputRecord.setField(1, coordinates);
+		
+		envelope.clear();
+		for(Coordinate cr : elem.getGeometry().getEnvelope().getCoordinates()) {
+			envelope.addPair(cr.x, cr.y);
+		}
+		outputRecord.setField(2, envelope);
+		
+		collector.collect(outputRecord);
+	}
+
+}
--- src/main/java/de/komoot/hackathon/ourcode/KomootStratosphere.java
+++ src/main/java/de/komoot/hackathon/ourcode/KomootStratosphere.java
@@ -1,8 +1,15 @@
 package de.komoot.hackathon.ourcode;
 
+import de.komoot.hackathon.ourcode.BBTask.PactCoordinatesList;
+import eu.stratosphere.pact.common.contract.FileDataSink;
+import eu.stratosphere.pact.common.contract.FileDataSource;
+import eu.stratosphere.pact.common.contract.MapContract;
+import eu.stratosphere.pact.common.io.RecordOutputFormat;
+import eu.stratosphere.pact.common.io.TextInputFormat;
 import eu.stratosphere.pact.common.plan.Plan;
 import eu.stratosphere.pact.common.plan.PlanAssembler;
 import eu.stratosphere.pact.common.plan.PlanAssemblerDescription;
+import eu.stratosphere.pact.common.type.base.PactString;
 
 public class KomootStratosphere implements PlanAssembler, PlanAssemblerDescription{
 	
@@ -12,9 +19,31 @@ public class KomootStratosphere implements PlanAssembler, PlanAssemblerDescripti
 	}
 
 	@Override
-	public Plan getPlan(String... arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public Plan getPlan(String... args) {
+		int noSubTasks = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
+		String dataInput = (args.length > 1 ? args[1] : "");
+		String output = (args.length > 2 ? args[2] : "");
+
+		FileDataSource source = new FileDataSource(TextInputFormat.class,
+				dataInput, "Input Lines");
+		source.setParameter(TextInputFormat.CHARSET_NAME, "ASCII"); // comment
+																	// out this
+																	// line for
+																	// UTF-8
+																	// inputs
+		
+		MapContract mapper = MapContract.builder(BBTask.class).input(source).name("BBTask").build();
+		
+			
+		FileDataSink out = new FileDataSink(RecordOutputFormat.class, output,
+		mapper, "Result");
+		RecordOutputFormat.configureRecordFormat(out).recordDelimiter('\n')
+		.fieldDelimiter(',').lenient(true).field(PactString.class, 0)
+		.field(PactCoordinatesList.class, 1).field(PactCoordinatesList.class, 2);
+
+		Plan plan = new Plan(out, "BB");
+		plan.setDefaultParallelism(noSubTasks);
+		return plan;
 	}
 
 }
